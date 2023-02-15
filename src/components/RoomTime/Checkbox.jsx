1956ffe2ffa489de7e02150dfb1813b5103152b3
daffaromero <daffaromero@mail.ugm.ac.id>
2023-02-15 15:51:10 +0700

    feat(CheckboxesHelper): finished table functionality

--- src/components/RoomTime/Checkbox.jsx
+++ src/components/RoomTime/Checkbox.jsx
@@ -62,12 +62,12 @@ export default function Checkbox({ value, onChange }) {
 
   return (
     <>
-      <div class="flex items-center mr-4">
+      <div class='flex items-center mr-4'>
         <input
           checked={isCheck}
-          type="checkbox"
+          type='checkbox'
           onChange={() => handleChange(value)}
-          className="w-5 h-5 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          className='w-5 h-5 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
         ></input>
       </div>
     </>
