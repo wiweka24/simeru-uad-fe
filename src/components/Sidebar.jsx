3b6d7d9310c9bce8887481cc7b329a23dee42bfa
daffaromero <daffaromero@mail.ugm.ac.id>
2023-03-16 17:30:51 +0700

    feat(Login+Sidebar): Log in with enter + Logout prompt

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -50,7 +50,7 @@ export default function Sidebar({ getAcadYearValue, acyear }) {
   // ];
 
   function SidebarTitle({ text }) {
-    return <div className="text-grey font-bold ml-3 py-2">{text}</div>;
+    return <div className='text-grey font-bold ml-3 py-2'>{text}</div>;
   }
 
   function SidebarLink({ Icon, text, linkto }) {
@@ -67,7 +67,7 @@ export default function Sidebar({ getAcadYearValue, acyear }) {
             activePage === linkto ? "stroke-[2.5px]" : "stroke-1.5px]"
           }`}
         />
-        <span className="inline truncate">{text}</span>
+        <span className='inline truncate'>{text}</span>
       </Link>
     );
   }
@@ -83,6 +83,27 @@ export default function Sidebar({ getAcadYearValue, acyear }) {
     } catch (err) {
       notifyError(err);
     }
+  }
+
+  function logoutCheck(e) {
+    Swal.fire({
+      html: `Anda yakin ingin keluar?`,
+      toast: true,
+      icon: "warning",
+      iconColor: "#000000",
+      showCancelButton: true,
+      cancelButtonText: "Tidak",
+      cancelButtonColor: "#991b1b",
+      confirmButtonText: "Ya",
+      confirmButtonColor: "#16a34a",
+      showClass: {
+        popup: "",
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        logoutSubmit(e);
+      }
+    });
   }
 
   async function logoutSubmit(e) {
@@ -110,59 +131,59 @@ export default function Sidebar({ getAcadYearValue, acyear }) {
   }
 
   return (
-    <div className="flex flex-col justify-between col-span-1 border-r">
+    <div className='flex flex-col justify-between col-span-1 border-r'>
       <div>
-        <div className="text-grey items-center px-2 mt-6 ml-3">
-          <div className="h-12 w-12 border rounded-full">
+        <div className='text-grey items-center px-2 mt-6 ml-3'>
+          <div className='h-12 w-12 border rounded-full'>
             <img
-              src="https://sbmptmu.id/wp-content/uploads/2022/03/Logo-UAD-Berwarna-1024x1024.png"
-              alt=""
-              className="p-1 rounded-full"
+              src='https://sbmptmu.id/wp-content/uploads/2022/03/Logo-UAD-Berwarna-1024x1024.png'
+              alt=''
+              className='p-1 rounded-full'
             />
           </div>
-          <div className="py-4 leading-5">
-            <h4 className="">Admin</h4>
-            <p className="text-xl text-grey-dark font-bold">Scheduler</p>
+          <div className='py-4 leading-5'>
+            <h4 className=''>Admin</h4>
+            <p className='text-xl text-grey-dark font-bold'>Scheduler</p>
           </div>
         </div>
 
-        <div className="px-2">
-          <SidebarTitle text="Input Data" />
-          <SidebarLink text="Matkul" Icon={BookOpenIcon} linkto="/MataKuliah" />
-          <SidebarLink text="Dosen" Icon={UserIcon} linkto="/Dosen" />
-          <SidebarLink text="Ruang Kelas" Icon={HomeIcon} linkto="/Ruangan" />
+        <div className='px-2'>
+          <SidebarTitle text='Input Data' />
+          <SidebarLink text='Matkul' Icon={BookOpenIcon} linkto='/MataKuliah' />
+          <SidebarLink text='Dosen' Icon={UserIcon} linkto='/Dosen' />
+          <SidebarLink text='Ruang Kelas' Icon={HomeIcon} linkto='/Ruangan' />
 
-          <SidebarTitle text="Konfigurasi" />
+          <SidebarTitle text='Konfigurasi' />
           <SidebarLink
-            text="Matkul Terselenggara"
+            text='Matkul Terselenggara'
             Icon={ClipboardDocumentCheckIcon}
-            linkto="/MKTerselenggara"
+            linkto='/MKTerselenggara'
           />
           <SidebarLink
-            text="Dosen Matkul"
+            text='Dosen Matkul'
             Icon={IdentificationIcon}
-            linkto="/DosenMatkul"
+            linkto='/DosenMatkul'
           />
           <SidebarLink
-            text="Ruang Kelas dan Waktu"
+            text='Ruang Kelas dan Waktu'
             Icon={ClockIcon}
-            linkto="/RuangWaktu"
+            linkto='/RuangWaktu'
           />
 
-          <SidebarTitle text="Jadwal" />
+          <SidebarTitle text='Jadwal' />
           <SidebarLink
-            text="Jadwal Matkul"
-            linkto="/Jadwal"
+            text='Jadwal Matkul'
+            linkto='/Jadwal'
             Icon={BookmarkIcon}
           />
-          <div className="mt-4">
-            <SidebarTitle text="Atur Tahun Ajaran" />
-            <div className="pl-2">
+          <div className='mt-4'>
+            <SidebarTitle text='Atur Tahun Ajaran' />
+            <div className='pl-2'>
               <Dropdown
                 label={acyear_string}
-                color="dark"
-                outline="false"
-                size="md"
+                color='dark'
+                outline='false'
+                size='md'
               >
                 {academicYear.map((acadyear) => (
                   <Dropdown.Item
@@ -179,32 +200,32 @@ export default function Sidebar({ getAcadYearValue, acyear }) {
         </div>
       </div>
 
-      <div className="px-2 mb-5">
-        <SidebarTitle text="Template Data Tahun Lalu" />
-        <div className="px-2">
+      <div className='px-2 mb-5'>
+        <SidebarTitle text='Template Data Tahun Lalu' />
+        <div className='px-2'>
           <Button
             text={
-              <div className="flex items-center">
-                <DocumentDuplicateIcon className="h-5 mr-1" />
+              <div className='flex items-center'>
+                <DocumentDuplicateIcon className='h-5 mr-1' />
                 Set Template Data
               </div>
             }
-            color="dark"
+            color='dark'
             onClick={postAcadYearTemplate}
           />
         </div>
       </div>
 
-      <div className="px-4 mb-5">
+      <div className='px-4 mb-5'>
         <Button
           text={
-            <div className="flex items-center">
-              <ArrowRightOnRectangleIcon className="h-5 mr-1" />
+            <div className='flex items-center'>
+              <ArrowRightOnRectangleIcon className='h-5 mr-1' />
               Logout
             </div>
           }
-          color="dark"
-          onClick={logoutSubmit}
+          color='dark'
+          onClick={logoutCheck}
         />
       </div>
     </div>
